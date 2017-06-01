f522d10824632a9f64a818e660772bd44221761a
CrystalTzh <1357627989@qq.com>
2017-06-01 11:13:58 +0800

    init

---
+++ HostelWorld/src/hostelworld/logic/RoomSaleManager.java
@@ -0,0 +1,42 @@
+package hostelworld.logic;
+
+import java.util.List;
+
+import hostelworld.model.HotelAccommocdatedStatistic;
+import hostelworld.model.RoomOrder;
+
+public interface RoomSaleManager {
+
+	/**
+	 * 获得会员和非会员的入住（已离店的）总数量
+	 * @return
+	 */
+	public int[] getAccommodatedNum();
+	
+	/**
+	 * 获得会员所有订单情况
+	 * @return
+	 */
+	public List<RoomOrder> getMemebrOrderList();
+	
+	/**
+	 * 找到所有已结算的会员订单
+	 * @return
+	 */
+	public List<RoomOrder> findAllPayedOrderList();
+	
+	/**
+	 * 获取所有酒店的finished的订单数据（已离店的）
+	 * @param
+	 * @return
+	 */
+	public List<HotelAccommocdatedStatistic> getHotelAccommocdatedStatistics();
+	
+	/**
+	 * 找到酒店的所有订单
+	 * @param hotelId
+	 * @return
+	 */
+	public List<RoomOrder> findOrder(String hotelId);
+	
+}
